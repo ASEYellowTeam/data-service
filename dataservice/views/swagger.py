cd0dc7461c6c96f5d4c23f9e58537f5e5e4fc876
fromgold <dallopollo@hotmail.it>
2018-11-29 09:49:47 +0100

    moved challenge stuff

--- dataservice/views/swagger.py
+++ dataservice/views/swagger.py
@@ -2,7 +2,7 @@ import os
 from datetime import datetime
 from flakon import SwaggerBlueprint
 from flask import request, jsonify, abort, make_response
-from dataservice.database import db, User, Run, Challenge
+from dataservice.database import db, User, Run
 
 HERE = os.path.dirname(__file__)
 YML = os.path.join(HERE, '..', 'static', 'api.yaml')
@@ -140,28 +140,3 @@ def get_run(run_id):
     if not run:
         abort(404)
     return run.to_json()
-
-
-@api.operation('getChallenges')
-def get_challenges(user_id):
-    challenges = db.session.query(Challenge).filter(Challenge.runner_id == user_id)
-    return jsonify([challenge.to_json() for challenge in challenges])
-
-
-@api.operation('createChallenge')
-def create_challenge(user_id):
-    req = request.json.items()
-    challenge = Challenge()
-    challenge.runner_id = user_id
-    challenge.run_one = req.run_one
-    challenge.name_run_one = req.name_run_one
-    challenge.run_two = req.run_two
-    challenge.name_run_two = req.name_run_two
-    db.session.commit()
-    return challenge.to_json()
-
-
-@api.operation('getChallenge')
-def get_challenge(challenge_id):
-    challenge = db.session.query(Challenge).filter(Challenge.id == challenge_id)
-    return challenge.to_json()
